4ed29632351d2bbcd76e9375903c277a8cd1a4c2
masha <marya_nep@mail.ru>
2026-01-09 19:53:39 +0500

    добавлена функциональность

---
+++ README.md
@@ -0,0 +1,1 @@
+https://github.com/macaronin01/to-do-pre
--- index.js
+++ index.js
@@ -6,13 +6,15 @@ let items = [
 	"Прогуляться по улице в солнечный день",
 	"Помыть посуду",
 ];
+console.log(localStorage.getItem('items'));
 
 const listElement = document.querySelector(".to-do__list");
 const formElement = document.querySelector(".to-do__form");
 const inputElement = document.querySelector(".to-do__input");
 
 function loadTasks() {
-
+  const saved = localStorage.getItem('items');
+  return saved ? JSON.parse(saved) : items;
 }
 
 function createItem(item) {
@@ -23,13 +25,56 @@ function createItem(item) {
   const duplicateButton = clone.querySelector(".to-do__item-button_type_duplicate");
   const editButton = clone.querySelector(".to-do__item-button_type_edit");
 
+  textElement.textContent = item;
+
+	deleteButton.addEventListener('click', function () {
+		clone.remove();
+		let items = getTasksFromDOM();
+		saveTasks(items);
+	});
+
+	duplicateButton.addEventListener('click', function () {
+		let itemName = textElement.textContent;
+		let newItem = createItem(item);
+		listElement.prepend(newItem);
+
+		let items = getTasksFromDOM();
+		saveTasks(items);
+	});
+
+	editButton.addEventListener('click', function () {
+		textElement.setAttribute("contenteditable", "true");
+		textElement.focus();
+	})
+
+	return clone;
 }
 
-function getTasksFromDOM() {
+items = loadTasks();
+items.forEach(item => {
+    const task = createItem(item);
+    listElement.append(task);
+});
 
+formElement.addEventListener('submit', function (evt) {
+  evt.preventDefault();
+	const inputText = inputElement.value;
+	listElement.prepend(createItem(inputText));
+	items = getTasksFromDOM();
+	saveTasks(items);
+  inputElement.value = '';
+});
+
+function getTasksFromDOM() {
+  const itemsNamesElements = document.querySelectorAll('.to-do__item-text');
+	let tasks = [];
+	itemsNamesElements.forEach(item => {
+		tasks.push(item.textContent);
+	});
+	return tasks;
 }
 
 function saveTasks(tasks) {
-
+  localStorage.setItem('items', JSON.stringify(tasks));
 }
 
